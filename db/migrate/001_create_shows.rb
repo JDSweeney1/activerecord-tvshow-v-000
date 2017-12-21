09071a618146c152a40e7733f3190746c8807e15
Jered Sweeney <sweeneyjered@gmail.com>
2017-12-21 02:20:12 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,6 +1,6 @@
 class CreateShows < ActiveRecord::migration
   def change
-    create_table :name do |s|
+    create_table :shows do |s|
       t.string  :name
       t.string  :network
       t.string  :day
